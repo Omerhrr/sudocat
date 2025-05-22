6779a7a6a33276bf7111b813e9c3fadb5e02a329
Omerhrr <pyomerhrr@gmail.com>
2025-05-22 16:42:28 +0100

    fourth commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,3 +11,12 @@ export default defineConfig({
   },
 })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     extensions: ['.tsx', '.ts', '.js', '.json'],
+//   },
+// })
